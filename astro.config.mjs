64219e7372de76074702e5109f322821eef6a6f8
slavenSusic <118363308+slavenSusic@users.noreply.github.com>
2023-12-15 07:02:50 +0100

    1111

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
